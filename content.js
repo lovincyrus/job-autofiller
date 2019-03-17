af0d4707b67ebc279de3570bae56aac2d5dc25cc
chriswongtv <chris@cogifire.com>
2019-03-16 22:17:00 -0700

    Standardize site data

--- content.js
+++ content.js
@@ -2,8 +2,7 @@ const AUTOFILL_DATA_FILE = chrome.runtime.getURL('data.json');
 const SITES_SELECTOR_MAPPING_FILE = chrome.runtime.getURL('sites.json');
 
 let autofillData = {};
-let supportedSites = [];
-let selectorMapping = {};
+let sitesData = {};
 
 /**
  * Import the data.json file
@@ -27,33 +26,72 @@ Promise.all([
   sitesStream
 ]).then(responses => {
   autofillData = responses[0];
-  supportedSites = responses[1].supportedSites;
-  selectorMapping = responses[1].selectorMapping;
+  sitesData = responses[1];
   init();
 })
+
+/**
+ * Checks if the hostname is supported.
+ * Finds a standardized equivalent if there's no direct match.
+ * @param {String} hostname - hostname of the current window
+ * @returns {String|null} - returns the standardized sitename or null
+ */
+function _isSupported(hostname) {
+  if (sitesData[hostname]) {
+    return hostname;
+  }
+  for (let site in supportedSites) {
+    if (hostname.includes(site)) {
+      return site;
+    }
+  }
+  return null;
+}
+
+/**
+ * Checks if the application type is simple or complex
+ * @param {String} siteName - standardized sitename
+ * @returns {Boolean}
+ */
+function _isComplex(siteName) {
+  return sitesData[siteName].isComplex;
+}
 
 /**
  * Initialize the autofill extension
  */
 function init() {
-  const hostname = window.location.hostname;
-  if (supportedSites.includes(hostname)) {
-    autofillPage(hostname);
+  const siteName = _isSupported(window.location.hostname);
+  if (!siteName) {
+    return;
+  }
+  if (_isComplex(siteName)) {
+    autofillComplexPages(siteName);
+  } else {
+    autofillPage(siteName);
   }
 }
 
 /**
  * Find the autofill-able input fields on the page according the hostname
- * @param {String} hostname - hostname of the current window
+ * @param {String} siteName - standardized sitename of the current window
  */
-function autofillPage(hostname) {
-  for (let selector in selectorMapping[hostname]) {
+function autofillPage(siteName) {
+  for (let selector in sitesData[siteName].selectorMapping) {
     const inputElement = $(selector);
     if (inputElement.length) {
-      const inputData = selectorMapping[hostname][selector];
+      const inputData = sitesData[siteName].selectorMapping[selector];
       autofillInput(inputElement, inputData);
     }
   }
+}
+
+/**
+ * Handles autofill for complex application pages
+ * @param {String} siteName - standardized sitename of the current window
+ */
+function autofillComplexPages(siteName) {
+  // TODO: Handle complex cases
 }
 
 /**
